e611ec14cc7aae934bedb1907309172d991bb775
Lucas Costa <olucascosta98@gmail.com>
2021-05-22 09:36:34 -0300

    feat: Reestruturando o projeto

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDatabase } from '../in-memory-database';
+import { ToastrModule } from 'ngx-toastr';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    ToastrModule.forRoot(),
+  ],
+  exports:[
+    //Shared modules
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+
+  ]
+})
+export class CoreModule { }
--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,23 +1,21 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { EntriesRoutingModule } from './entries-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 
 import{ CalendarModule}  from 'primeng/calendar';
 import { IMaskModule } from 'angular-imask';
+import { SharedModule } from 'src/app/shared/shared.module';
 @NgModule({
   declarations: [
     EntryListComponent,
     EntryFormComponent,
   ],
   imports: [
-    CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     CalendarModule,
     IMaskModule,
+    SharedModule
   ]
 })
 export class EntriesModule { }
