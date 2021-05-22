f361200126fd82dd837049e324ff601af4de422a
Lucas Costa <olucascosta98@gmail.com>
2021-05-21 21:36:17 -0300

    feat: Adiciona category form

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { EntriesRoutingModule } from './entries-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { EntryListComponent } from './entry-list/entry-list.component';
-
+import { EntryFormComponent } from './entry-form/entry-form.component';
 
 @NgModule({
   declarations: [
-    EntryListComponent
+    EntryListComponent,
+    EntryFormComponent,
   ],
   imports: [
     CommonModule,
