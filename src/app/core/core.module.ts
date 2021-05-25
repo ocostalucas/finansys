3195452e7b4bb016c73db1665300fc0dd5e7b177
Lucas Costa <olucascosta98@gmail.com>
2021-05-24 23:04:10 -0300

    refactor: Adiciona NavbarComponent

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,9 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDatabase } from '../in-memory-database';
 import { ToastrModule } from 'ngx-toastr';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule,
     BrowserModule,
@@ -16,12 +20,14 @@ import { ToastrModule } from 'ngx-toastr';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
     ToastrModule.forRoot(),
+    RouterModule,
   ],
   exports:[
     //Shared modules
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NavbarComponent,
 
   ]
 })
